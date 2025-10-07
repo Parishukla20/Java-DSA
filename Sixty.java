d8244088ee827853f7d9352dddc7c8df0f13e605
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2025-10-07 21:38:58 +0530

    Add files via upload

---
+++ Sixty.java
@@ -0,0 +1,27 @@
+//Sorting in java
+//Bubble Sort Code
+
+public class Sixty {
+    public static void printArray(int arr[]){
+        for(int i =0;i<arr.length;i++){
+           System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[] = {5,4,3,2,1};
+
+        //outer loop for n-1 times
+        for(int i=0;i<arr.length-1;i++){
+            //inner loop
+            for(int j=0;j<arr.length-i-1;j++){    
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
