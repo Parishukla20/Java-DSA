789c9f83176eeb49ae0eabf6d9995c1cb35012c6
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2025-10-11 19:50:09 +0530

    Add files via upload

---
+++ SixtyThree.java
@@ -0,0 +1,16 @@
+//Recursion in java
+//Print number from 5 to 1
+
+public class SixtyThree {
+    public static void printNum(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        printNum(n-1);
+    }
+    public static void main(String args[]){
+           int n = 5;
+           printNum(n);
+    }
+}
