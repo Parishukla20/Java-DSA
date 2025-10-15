696a687b8c907f1db92a8e9e5e3f7bfbd9ba29eb
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2025-10-15 22:04:23 +0530

    Add files via upload

---
+++ SixtySix.java
@@ -0,0 +1,18 @@
+//Print factorial of a number n using recursion
+
+public class SixtySix {
+    public static int printFact(int n){
+        if(n==1||n==0){
+            return 1;
+        }
+        int fact_nm1 = printFact(n-1);
+        int fact_n = n*fact_nm1;
+        return fact_n;
+    }
+    public static void main(String args[]){
+        int n =5;
+        printFact(n);
+        int ans = printFact(n);
+        System.out.println(ans);
+    }
+}
