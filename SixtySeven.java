3ec6052330c7669d8c6f9f177b3bef4e883d8b46
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2025-10-15 22:54:10 +0530

    Add files via upload

---
+++ SixtySeven.java
@@ -0,0 +1,19 @@
+//Print the fibonacci sequence till nth term
+
+public class SixtySeven {
+    public static void fibonacci(int a,int b,int n){
+        if(n==0){
+            return;
+        }
+        int c = a+b;
+        System.out.println(c);
+        fibonacci(b, c, n-1);
+    }
+    public static void main(String args[]){
+        int a=0, b=1;
+        System.out.println(a);
+        System.out.println(b);
+        int n =10;
+        fibonacci(a,b,n-2);
+    }
+}
