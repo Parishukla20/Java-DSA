ac1f9b3b12044eb730117b80c2fcf3565df7033b
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2025-11-04 21:12:56 +0530

    Add files via upload

---
+++ EightyNine.java
@@ -0,0 +1,32 @@
+//OOPS in Java
+
+class Student{
+    String name;
+    int rollNo;
+    public void name(){
+        System.out.println(this.name);
+    }
+    public void rollNo(){
+        System.out.println(this.rollNo);
+    }
+    public void Study(){
+        System.out.println("I am reading");
+    }
+}
+public class EightyNine {
+    public static void main(String args[]){
+        Student stu1 = new Student();
+        Student stu2 = new Student();
+        stu1.name ="Pari Shukla";
+        stu1.rollNo = 10;
+        stu2.name = "Prerna Shukla";
+        stu2.rollNo = 11;
+
+        stu1.name();
+        stu1.rollNo();
+        stu1.Study();
+        stu2.name();
+        stu2.rollNo();
+        stu2.Study();
+    }
+}
