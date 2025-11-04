8c0ee194504fdc1afb25279681dd3a6c3a695213
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2025-11-04 21:04:03 +0530

    Update EightyEight.java

--- EightyEight.java
+++ EightyEight.java
@@ -7,6 +7,12 @@ class Pen{
     public void write(){
         System.out.println("writing something");
     }
+    public void printColor(){
+        System.out.println(this.color);
+    }
+    public void printType(){
+        System.out.println(this.type);
+    }
 }
 public class EightyEight {
     public static void main(String args[]){
@@ -14,6 +20,16 @@ public class EightyEight {
         pen1.color = "blue";
         pen1.type = "gel";
 
+        Pen pen2 = new Pen();
+        pen2.color = "black";
+        pen2.type = "ball";
+        
+        pen1.printType();
         pen1.write();
+        pen1.printColor();
+        pen2.printType();
+        pen2.write();
+        pen2.printColor();
     }
 }
+
