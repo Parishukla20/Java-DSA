1031b81fe7b31af6d94bf55ca146ae8cdf27d34d
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2025-11-07 21:47:05 +0530

    Add files via upload

---
+++ NinetyThree.java
@@ -0,0 +1,14 @@
+//Inheritence in java
+
+class Pen{
+    String color;
+}
+class Blue extends Pen{
+    
+}
+public class NinetyThree {
+    public static void main(String args[]){
+        Blue blue1 = new Blue();
+        blue1.color = "blue";
+    }
+}
