96f6536c2896f17c5e87b06dd15a082e2159c29a
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2025-11-27 21:00:04 +0530

    Add files via upload

---
+++ HundredThree.java
@@ -0,0 +1,31 @@
+//Linked List operations with functions
+
+import java.util.*;
+public class HundredThree {
+    public static void main(String args[]){
+        LinkedList<String> list = new LinkedList<String>();
+
+        list.addFirst("a");
+        list.addFirst("is");
+        System.out.println(list);
+        list.addFirst("this");
+        System.out.println(list);
+        list.add("List"); //by default it will add to the last
+        System.out.println(list);
+        System.out.println(list.size());
+
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i)+" -> ");
+        }
+        System.out.println("null");
+
+        list.removeFirst();
+        System.out.println(list);
+
+        list.removeLast();
+        System.out.println(list);
+
+        list.remove(1);
+        System.out.println(list);
+    }
+}
