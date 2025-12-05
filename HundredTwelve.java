4dea3a4b36a225dcc73756ee72cf4d0651a81aa2
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2025-12-05 21:42:39 +0530

    Create HundredTwelve.java

---
+++ HundredTwelve.java
@@ -0,0 +1,19 @@
+//Stack using java built in Class(java.util.stack)
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Top element: " + stack.peek());
+
+        System.out.println("Popped: " + stack.pop());
+        System.out.println("Popped: " + stack.pop());
+
+        System.out.println("Is empty? " + stack.isEmpty());
+    }
+}
