3501a98e5c90cd2ed3c0820edb1d3d581a5bc656
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2026-02-11 22:18:01 +0530

    Update Eighteen.java

--- Eighteen.java
+++ Eighteen.java
@@ -4,11 +4,12 @@ public class Eighteen {
     public static void main(String args[]){
         for(int i=1;i<=4;i++){
             for(int j=1;j<=5;j++){
-                System.out.print("*");//print *
+                System.out.print("*");
             }
-            System.out.println("");  //space   
+            System.out.println("");  
         }
     }
 }
+
 
 
