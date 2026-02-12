97b473737edb625210c811ea3427a6fe71064b12
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2026-02-12 22:12:46 +0530

    Add files via upload

---
+++ HundredThirtyNine.java
@@ -0,0 +1,18 @@
+//Hashset basics
+
+import java.util.HashSet;
+import java.util.Iterator;
+public class HundredThirtyNine {
+    public static void main(String argds[]){
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        System.out.println(set);
+        //Iterator
+        Iterator it = set.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}
