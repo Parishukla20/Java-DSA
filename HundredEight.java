61f03e4e58621c3855eca786f5bfede635501fc9
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2026-08-06 20:51:41 +0530

    Update HundredEight.java

--- HundredEight.java
+++ HundredEight.java
@@ -17,7 +17,6 @@ public class HundredEight {
         // Step 1: get the string of all values
         String s = "";
         ListNode temp = head;
-
         while(temp != null){
             s = s + temp.val;
             temp = temp.next;
@@ -26,7 +25,6 @@ public class HundredEight {
         // Step 2: check palindrome
         int i = 0;
         int j = s.length() - 1;
-
         while(i < j){
             if(s.charAt(i) != s.charAt(j)){
                 return false;
@@ -34,20 +32,16 @@ public class HundredEight {
             i++;
             j--;
         }
-
         return true;
     }
 
     public static void main(String[] args) {
-
         // Creating list: 1 -> 2 -> 2 -> 1
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(2);
         head.next.next.next = new ListNode(1);
-
         HundredEight obj = new HundredEight();
-
         boolean ans = obj.isPalindrome(head);
 
         if(ans){
